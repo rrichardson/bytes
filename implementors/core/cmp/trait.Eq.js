72e9b2823133e08e7d1fce66a095a357e93f9506
Carl Lerche <me@carllerche.com>
2016-06-08 21:49:35 +0000

    rebuild pages at 270d2e2

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bytes'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='bytes/struct.Bytes.html' title='bytes::Bytes'>Bytes</a>",];
+implementors["bytes"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/cmp/trait.Eq.html' title='core::cmp::Eq'>Eq</a> for <a class='struct' href='bytes/struct.Bytes.html' title='bytes::Bytes'>Bytes</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bytes'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/struct.RingBuf.html' title='bytes::RingBuf'>RingBuf</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/struct.MutByteBuf.html' title='bytes::MutByteBuf'>MutByteBuf</a>",];
+implementors["bytes"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/struct.MutByteBuf.html' title='bytes::MutByteBuf'>MutByteBuf</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
