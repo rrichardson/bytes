068a3c79feb087aa7855d7e4485772882b376930
Carl Lerche <me@carllerche.com>
2016-06-19 20:33:26 +0000

    rebuild pages at 3ca0095

--- implementors/core/marker/trait.Copy.js
+++ implementors/core/marker/trait.Copy.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bytes'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='bytes/str/struct.SmallByteStr.html' title='bytes::str::SmallByteStr'>SmallByteStr</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='bytes/enum.BufError.html' title='bytes::BufError'>BufError</a>",];
+implementors["bytes"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='bytes/struct.SmallByteStr.html' title='bytes::SmallByteStr'>SmallByteStr</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='enum' href='bytes/enum.BufError.html' title='bytes::BufError'>BufError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
