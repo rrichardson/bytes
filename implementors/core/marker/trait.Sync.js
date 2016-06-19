94884306e7c10438cc477f10cab73cbb7a6bbd0b
Carl Lerche <me@carllerche.com>
2016-06-19 20:33:34 +0000

    rebuild pages at 3ca0095

--- implementors/core/marker/trait.Sync.js
+++ implementors/core/marker/trait.Sync.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bytes"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='bytes/alloc/struct.MemRef.html' title='bytes::alloc::MemRef'>MemRef</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='bytes/struct.Bytes.html' title='bytes::Bytes'>Bytes</a>",];
+implementors['bytes'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='bytes/alloc/struct.MemRef.html' title='bytes::alloc::MemRef'>MemRef</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Sync.html' title='core::marker::Sync'>Sync</a> for <a class='struct' href='bytes/str/struct.Bytes.html' title='bytes::str::Bytes'>Bytes</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
