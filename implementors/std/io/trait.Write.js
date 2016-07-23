3929d7ff42aa108e2c570beb8f93046621d44f28
Carl Lerche <me@carllerche.com>
2016-07-23 17:01:27 +0000

    rebuild pages at eec203c

--- implementors/std/io/trait.Write.js
+++ implementors/std/io/trait.Write.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bytes'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/buf/struct.MutByteBuf.html' title='bytes::buf::MutByteBuf'>MutByteBuf</a>",];
+implementors["bytes"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/io/trait.Write.html' title='std::io::Write'>Write</a> for <a class='struct' href='bytes/struct.MutByteBuf.html' title='bytes::MutByteBuf'>MutByteBuf</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
